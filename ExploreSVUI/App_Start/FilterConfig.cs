891b52e79798e3d4b5a2460aff04e7f9188b7aaf
Alejandro Juarez <ja24001@esfe.agape.edu.sv>
2025-02-17 16:14:09 -0600

    Agregar archivos de proyecto.

---
+++ ExploreSVUI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ExploreSVUI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ExploreSVUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExploreSVUI.MvcApplication" Language="C#" %>
